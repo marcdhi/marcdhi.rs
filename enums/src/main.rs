143c31eea35fec94558d48e5ed39a66671f88dc6
Mardav Chirag Gandhi <gandhi.mardav@gmail.com>
2023-11-13 22:59:55 +0530

    rust : add enums

---
+++ enums/src/main.rs
@@ -0,0 +1,38 @@
+// fn main() {
+//     enum Message {
+//         Quit,
+//         Move { x: i32, y: i32 },
+//         Write(String),
+//         ChangeColor(i32, i32, i32),
+//     }
+
+//     impl Message {
+//         fn call(&self) {
+//             // method body would be defined here
+//         }
+//     }
+
+//     let m = Message::Write(String::from("hello"));
+//     m.call();
+// }
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn main() {}
